a36d4da2d20eb2ff12d866abbffc819c0a82ea0d
Varun Vachhar <varunvachhar@gmail.com>
2014-07-22 13:33:39 -0400

    [feature] dynamic example for common header

--- www/components/common-header/common-header.js
+++ www/components/common-header/common-header.js
@@ -7,20 +7,18 @@ angular.module('commonHeader', [])
       return {
         restrict: 'E',
         templateUrl: 'components/common-header/common-header.html',
-        scope: true,
-        link: function(scope, iElement, iAttrs) {
-          scope.authStatus = scope.$eval(iAttrs.authStatus);
-          scope.isPurchaser = scope.$eval(iAttrs.isPurchaser);
-          scope.companyLoaded = scope.$eval(iAttrs.companyLoaded);
-          scope.selectedCompanyName = scope.$eval(iAttrs.selectedCompanyName);
-          scope.canChangeCompany = scope.$eval(iAttrs.canChangeCompany);
-
-          scope.messages = ['<li class="system-message">Lorem ipsum dolor sit amet</li>', '<li class="system-message">Consectetur adipiscing elit</li>', '<li class="system-message">Suspendisse convallis leo sed</li>'];
-          // User
-          scope.userProfilePicture = scope.$eval(iAttrs.userProfilePicture);
-          scope.userProfileName = scope.$eval(iAttrs.userProfileName);
-          scope.userProfileEmail = scope.$eval(iAttrs.userProfileEmail);
-        }
+        scope: {
+          authStatus: '=',
+          isPurchaser: '=',
+          companyLoaded: '=',
+          selectedCompanyName: '=',
+          canChangeCompany: '=',
+          userProfilePicture: '=',
+          userProfileName: '=',
+          userProfileEmail: '=',
+          messages: '='
+        },
+        link: function(scope, iElement, iAttrs) {}
       };
     }
   ]);
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,5 +7,18 @@ angular.module('styleGuideApp.controllers')
     }
   ])
   .controller('CommonHeaderCtrl', ['$scope',
-    function($scope) {}
+    function($scope) {
+      $scope.authStatus = 1;
+      $scope.isPurchaser = true;
+      // Company
+      $scope.companyLoaded = true;
+      $scope.canChangeCompany = true;
+      $scope.selectedCompanyName = 'Rise Vision';
+      // User
+      $scope.userProfileName = 'Alfredo Sanchez';
+      $scope.userProfileEmail = 'alfredo.sanchez90@example.com';
+      $scope.userProfilePicture = 'http://api.randomuser.me/portraits/med/men/33.jpg';
+      // Messages
+      $scope.messages = ['<li class="system-message">Lorem ipsum dolor sit amet</li>', '<li class="system-message">Consectetur adipiscing elit</li>', '<li class="system-message">Suspendisse convallis leo sed</li>'];
+    }
   ]);
