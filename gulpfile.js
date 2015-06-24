ab9d23474c975e383ca40af80fe576adfe71af7b
Ghislaine Guerin <me@ghislaineguerin.com>
2015-06-24 17:03:14 -0400

    landing page styles

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var paths = {
   appSass: './src/app.scss',
   alignmentSass: './src/ui-components/alignment.scss',
   distFonts: './dist/fonts',
-  distCss: './dist/css',
+  distCss: '/Users/ghislaineguerin/Documents/ux-mockups/bower_components/rv-common-style/dist/css',
   fonts: ['./bower_components/font-awesome/fonts/*.*','./bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*.*']
 };
 
